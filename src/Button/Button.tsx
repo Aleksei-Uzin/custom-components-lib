c035e9e1fc2c310acc860f11f8127ad165369e6d
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-10-09 20:20:39 +0300

    feat: create Button component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,44 @@
+import classNames from 'classNames'
+
 import { ButtonProps } from './types'
 
-export function Button({ children, className }: ButtonProps) {
-  return <button className={className}>{children}</button>
+export function Button({
+  children,
+  className,
+  color = 'primary',
+  disabled,
+  fullWidth,
+  loading,
+  onClick,
+  size = 'medium',
+  style,
+  type = 'button',
+  variant = 'text',
+  ...props
+}: ButtonProps) {
+  const finalClassNames = classNames(
+    'ui-button',
+    {
+      // [styles.button]: true,
+      // [styles[color]]: true,
+      // [styles.disabled]: disabled,
+      // [styles.fullWidth]: fullWidth,
+      // [styles[size]]: true,
+      // [styles[variant]]: true,
+    },
+    className
+  )
+
+  return (
+    <button
+      className={finalClassNames}
+      disabled={disabled || loading}
+      onClick={onClick}
+      style={style}
+      type={type}
+      {...props}
+    >
+      {children}
+    </button>
+  )
 }
--- src/Button/types.ts
+++ src/Button/types.ts
@@ -1,7 +1,20 @@
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  /** The text or content to display inside the button */
-  children: React.ReactNode
-  /** Optional CSS class names for styling */
-  className?: string
+import { SharedBasic } from '../utils'
+
+export interface ButtonProps extends SharedBasic {
+  /** Button primary color */
+  color?: 'primary' | 'info' | 'success' | 'warning' | 'error'
+  /** Whether the button is disabled */
+  disabled?: boolean
+  /**	If true, the button will take up the full width of its container */
+  fullWidth?: boolean
+  /** Loading state */
+  loading?: boolean
+  /** Optional click event handle */
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+  /** Sets the size of the button */
+  size?: 'small' | 'medium' | 'large'
+  /** The type of the button */
+  type?: 'button' | 'submit' | 'reset'
+  /** The variant to use */
+  variant?: 'contained' | 'outlined' | 'text'
 }

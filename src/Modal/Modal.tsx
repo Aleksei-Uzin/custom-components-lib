997848deb0ebe4a4f80ce694a7d70584952cf234
Aleksei-Uzin <aleksei.uzin@gmail.com>
2025-10-15 13:38:48 +0300

    refactor: Modal component

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,24 +1,54 @@
-import cx from 'classnames'
-import { ModalHeader } from './ModalHeader'
+import { useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+import { Button } from '../Button'
 import { ModalProps } from './types'
+import cx from 'classnames'
+
 import '../Color'
 import styles from './Modal.module.css'
 
-export function Modal({ children, className, isOpen }: ModalProps) {
-  const finalModalClassNames = cx(
-    styles.modal,
-    {
-      [styles.displayBlock]: isOpen,
-      [styles.displayNone]: !isOpen,
-    },
-    className
-  )
+export const Modal: React.FC<ModalProps> = ({
+  container,
+  children,
+  modalClassName,
+  headerClassName,
+  contentClassName,
+  open: isOpen,
+  onClose,
+  title,
+  ...rest
+}) => {
+  const dialogRef = useRef<HTMLDialogElement>(null)
 
-  return (
-    <div className={finalModalClassNames}>
-      <div className={styles.modalContent}>{children}</div>
-    </div>
+  useEffect(() => {
+    if (isOpen) {
+      dialogRef.current?.showModal()
+    } else {
+      dialogRef.current?.close()
+    }
+  }, [isOpen])
+
+  const finalModalClassNames = cx(styles.modal, modalClassName)
+  const finalHeaderClassNames = cx(styles.header, headerClassName)
+  const finalContentClassNames = cx(styles.content, contentClassName)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    onClose()
+  }
+
+  if (!isOpen) return null
+
+  return createPortal(
+    <dialog ref={dialogRef} className={finalModalClassNames} onCancel={onClose} {...rest}>
+      <header className={finalHeaderClassNames}>
+        <span>{title}</span>
+        <Button color="info" size="small" onClick={handleClick} variant="outlined">
+          Close
+        </Button>
+      </header>
+      <div className={finalContentClassNames}>{children}</div>
+    </dialog>,
+    container
   )
 }
-
-Modal.Header = ModalHeader
